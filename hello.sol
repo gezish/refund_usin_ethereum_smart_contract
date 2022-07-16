b7e7d470245a5fa6144a81532d68213db20f12b6
gezish <enggezahegn.w@gmail.com>
2022-07-16 23:33:47 +0300

    all artifaccts

--- hello.sol
+++ hello.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-  
-contract HelloWorld {
-  string public yourName ;
-  constructor() public {
+
+contract hello {
+    string public yourName ;
+
+
+    constructor() public {
         yourName = "Unknown" ;
 }
 
 function setName(string memory nm) public{
-        yourName = nm ;
+        yourName = nm;
 }
-
+  
 }
-
-
 
